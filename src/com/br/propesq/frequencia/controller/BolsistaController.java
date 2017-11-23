c9ba9a79e64d2b1e61de2b6f874a8e260049cf6d
Thayane Lopes <tl.1004@outlook.com>
2017-11-23 02:05:04 -0200

    Testando mensagem de sucesso

--- src/com/br/propesq/frequencia/controller/BolsistaController.java
+++ src/com/br/propesq/frequencia/controller/BolsistaController.java
@@ -2,8 +2,11 @@ package com.br.propesq.frequencia.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.br.propesq.frequencia.Dao.BolsistaHibernateDao;
@@ -13,6 +16,30 @@ import com.br.propesq.frequencia.model.Campus;
 
 @Controller
 public class BolsistaController {
+	
+	
+	@RequestMapping("/cadastroComSucessoVoluntario")
+	public String cadastroComSucessoBolsista( Bolsista bolsista, BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
+			return "forward:cadastroBolsista";
+		}
+
+		BolsistaHibernateDao dao = new BolsistaHibernateDao();
+//		try {
+//			Perfil perfil = Perfil.BOLSISTA;
+//			System.out.println(perfil.getvalor());
+//			bolsista.setPerfil(perfil);
+//			dao.inserirVoluntario(bolsista);
+//			model.addAttribute("msg", "Usuario incluido com Sucesso!");
+//		} catch (ElementoJaExistenteException e) {
+//
+//			model.addAttribute("mensagem", "Usuario já existente");
+//		}
+		return "forward:cadastroBolsista";
+		
+	}
+
 
 	@RequestMapping("cadastroBolsista")
 	public String cadastroBolsista(Bolsista bolsista, Model model) {

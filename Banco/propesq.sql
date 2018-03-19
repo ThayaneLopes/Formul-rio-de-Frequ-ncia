0d6a1262892d9b49a575378aa48b7757c74d8b47
Thayane Lopes <tl.1004@outlook.com>
2018-03-19 08:54:45 -0300

    Att banco

--- Banco/propesq.sql
+++ Banco/propesq.sql
@@ -1,3 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 19-Mar-2018 às 12:51
+-- Versão do servidor: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `propesq`
 --
@@ -29,28 +49,13 @@ CREATE TABLE `bolsista` (
   `id_orientador` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(150) NOT NULL,
-  `login` varchar(100) NOT NULL,
-  `cpf` varchar(14) NOT NULL,
-  `rg` varchar(14) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `senha` varchar(100) NOT NULL,
-  `campus` int(11) DEFAULT NULL,
-  `tipo_usuario` varchar(400) DEFAULT NULL
-  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 --
 -- Extraindo dados da tabela `bolsista`
 --
 
 INSERT INTO `bolsista` (`id`, `nome`, `matricula`, `login`, `cpf`, `rg`, `historico_escolar`, `curriculo`, `titulo_plano`, `email`, `telefone`, `agencia`, `conta`, `senha`, `campus`, `tipo_projeto`, `banco`, `id_orientador`) VALUES
-(1, 'Thayane Lopes', '1234567891012', 'thayane', '999.999.999-99', '12344994', '417583671350a2cce56f8e2e7c8b1093--cone-dos-desenhos-animados-bola-de-natal-15.png', 'www', 'plano 1', 'teste@email.com', '(81) 97908-2434', '0123-455', '000000-0', 'sha1:64000:18:p0CNmAURMtMUXC4lNaubAZ0Ln0mt8Hwj:cH5lk0B+3J0gHVZIgRJZK8sj', 3, 'PIBITI GRAD CNPq', 'banco do brasil', 1),
-(3, 'Thayane Lopes', '1234567891012', 'sandra', '999.999.999-99', '12344994', 'd8fd0ecf239e0007d54bb163b3ce18de.jpg', 'www', 'ffff', 'teste@email.com', '(81) 97908-2434', '0123-455', '000000-0', 'sha1:64000:18:qvn8RDmzMkBRtfJtvLnoHoEDULkUjyPi:QAh6NMQ8p70kQ+ddbSyecvIu', 3, 'PIBIC AF IFPE', 'banco do brasil', 1);
+(9, 'Thayane Lopes', '20152tijg0644', 'thayane', '333.333.333-33', '2222222', 'ok.pdf', 'wwww', 'plano qualquer', 'teste@email.com', '(81) 99999-9999', '3333-333', '333333-3', '25f9e794323b453885f5181f1b624d0b', 5, 'PIBIC GRAD IFPE', 'Caixa', 1),
+(10, 'Sandra Lopes', '1234567891012', 'sandra', '999.999.999-99', '8888888888888', 'Briefing.docx', 'wwww', 'plano a', 'sandralopes1973@hotmail.com', '(81) 99999-9999', '8888-888', '888888-8', '25f9e794323b453885f5181f1b624d0b', 3, 'PIBITI GRAD CNPq', 'caixa', 3);
 
 -- --------------------------------------------------------
 
@@ -61,7 +66,7 @@ INSERT INTO `bolsista` (`id`, `nome`, `matricula`, `login`, `cpf`, `rg`, `histor
 CREATE TABLE `campus` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `telefone` varchar(14) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
   `logradouro` varchar(100) NOT NULL,
   `numero_logradouro` int(11) NOT NULL,
   `complemento` varchar(150) NOT NULL,
@@ -77,8 +82,9 @@ CREATE TABLE `campus` (
 --
 
 INSERT INTO `campus` (`id`, `nome`, `telefone`, `logradouro`, `numero_logradouro`, `complemento`, `bairro`, `cidade`, `estado`, `cep`, `numero`) VALUES
-(3, 'Jaboat�o dos Guararapes', '81979082153', 'rus tsd', 158, '', 'Barra de Jangada', 'jaboatao', 'PE', '54.490-000', NULL),
-(4, 'Jaboat�o dos Guararapes', '81979082535', 'sddddddddddddddssssssssss', 22222, 'sdddddddddddd', 'dssssssssssssssssssssss', 'fdgdgfdsdddddddd', 'AL', '54490000', NULL);
+(3, 'Jaboatão dos Guararapes', '81979082153', 'rus tsd', 158, '', 'Barra de Jangada', 'jaboatao', 'PE', '54.490-000', NULL),
+(4, 'Jaboatão dos Guararapes', '81979082535', 'sddddddddddddddssssssssss', 22222, 'sdddddddddddd', 'dssssssssssssssssssssss', 'fdgdgfdsdddddddd', 'AL', '54490000', NULL),
+(5, 'Recife', '(33) 33333-3333', 'aaaaaaa', 123, '', 'sdddddd', 'Recife', 'PE', '22.222-222', NULL);
 
 -- --------------------------------------------------------
 
@@ -99,27 +105,23 @@ CREATE TABLE `formulario_frequencia` (
   `pagamento_bolsa` varchar(15) DEFAULT NULL,
   `comentarios_estudante` varchar(150) DEFAULT NULL,
   `comentarios_orientador` varchar(150) DEFAULT NULL,
-  `data_entrega` varchar(15) DEFAULT NULL,
-  `mesAno` varchar(255) DEFAULT NULL,
-  `nome_bolsista` varchar(150) DEFAULT NULL,
-  `matricula_bolsista` varchar(14) DEFAULT NULL,
-  `titulo_plano` varchar(150) DEFAULT NULL,
-  `tipo_projeto` varchar(100) DEFAULT NULL,
-  `nome_orientador` varchar(150) DEFAULT NULL,
+  `data_entrega` date DEFAULT NULL,
   `status_bolsista` tinyint(1) DEFAULT NULL,
   `status_orientador` tinyint(1) DEFAULT NULL,
   `status_gestor` tinyint(1) DEFAULT NULL,
-  `status_propesq` tinyint(1) DEFAULT NULL
+  `status_propesq` tinyint(1) DEFAULT NULL,
+  `mesAno` varchar(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `formulario_frequencia`
 --
 
-INSERT INTO `formulario_frequencia` (`id`, `id_bolsista`, `id_usuario`, `situacao_cronograma`, `justificativa`, `resumo_atividades`, `carga_horaria`, `interesse_atividades`, `progresso_alcancado`, `pagamento_bolsa`, `comentarios_estudante`, `comentarios_orientador`, `data_entrega`, `mesAno`, `nome_bolsista`, `matricula_bolsista`, `titulo_plano`, `tipo_projeto`, `nome_orientador`, `status_bolsista`, `status_orientador`, `status_gestor`, `status_propesq`) VALUES
-(1, 1, 1, 'eeee', 'eee', 'ee', NULL, NULL, NULL, NULL, 'eee', 'eeee', '2018-01-17', NULL, 'thayane', 'sdfsg', 'sddsd', 'sdfsdfd', 'dsffdsf', NULL, NULL, NULL, NULL);
+INSERT INTO `formulario_frequencia` (`id`, `id_bolsista`, `id_usuario`, `situacao_cronograma`, `justificativa`, `resumo_atividades`, `carga_horaria`, `interesse_atividades`, `progresso_alcancado`, `pagamento_bolsa`, `comentarios_estudante`, `comentarios_orientador`, `data_entrega`, `status_bolsista`, `status_orientador`, `status_gestor`, `status_propesq`, `mesAno`) VALUES
+(13, 10, NULL, NULL, NULL, 'aaaa', NULL, NULL, NULL, NULL, 'bbbbb', NULL, '2018-03-16', 1, NULL, NULL, NULL, '');
 
 -- --------------------------------------------------------
+
 
 --
 -- Estrutura da tabela `tipo_usuario`
@@ -135,9 +137,10 @@ CREATE TABLE `tipo_usuario` (
 --
 
 INSERT INTO `tipo_usuario` (`id`, `descricao`) VALUES
-(1, 'Orientador'),
-(3, 'Gestor'),
-(4, 'Propesq');
+(1, 'orientador'),
+(3, 'gestor'),
+(4, 'propesq'),
+(5, 'admin');
 
 --
 -- Indexes for dumped tables
@@ -151,13 +154,6 @@ ALTER TABLE `bolsista`
   ADD KEY `campus` (`campus`),
   ADD KEY `cpf` (`cpf`) USING BTREE,
   ADD KEY `id_orientador` (`id_orientador`);
-  
-  
-  ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `campus` (`campus`),[
-   ADD KEY `tipo_usuario` (`tipo_usuario`),
-  ADD KEY `cpf` (`cpf`) USING BTREE;
 
 --
 -- Indexes for table `campus`
@@ -187,22 +183,26 @@ ALTER TABLE `tipo_usuario`
 -- AUTO_INCREMENT for table `bolsista`
 --
 ALTER TABLE `bolsista`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `campus`
 --
 ALTER TABLE `campus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `formulario_frequencia`
 --
 ALTER TABLE `formulario_frequencia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
 -- AUTO_INCREMENT for table `tipo_usuario`
 --
 ALTER TABLE `tipo_usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- Constraints for dumped tables
 --
@@ -211,8 +211,59 @@ ALTER TABLE `tipo_usuario`
 -- Limitadores para a tabela `bolsista`
 --
 ALTER TABLE `bolsista`
-  ADD CONSTRAINT `bolsista_ibfk_1` FOREIGN KEY (`campus`) REFERENCES `campus` (`id`),
-  ADD CONSTRAINT `id_orientador` FOREIGN KEY (`id_orientador`) REFERENCES `usuario` (`id`);
+  ADD CONSTRAINT `bolsista_ibfk_1` FOREIGN KEY (`campus`) REFERENCES `campus` (`id`);
+COMMIT;
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(150) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `rg` varchar(14) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `campus` int(11) DEFAULT NULL,
+  `tipo_usuario` varchar(400) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `login`, `cpf`, `rg`, `email`, `telefone`, `senha`, `campus`, `tipo_usuario`) VALUES
+(1, 'Thayane Lopes', 'thayane', '333.333.333-33', '12344994', 'teste@email.com', '(88) 88888-8888', 'sha1:64000:18:KFMDEIGTUBakpgZ3KiQ0qYzrr9IFq2Ii:+zl5WJiArebCO10aH8nNk4C0', 4, '1'),
+(2, 'Elinda', 'thayane', '999.999.999-99', '99999999999999', 'fndsihnf@iwfhfisu', '(99) 99999-9999', 'sha1:64000:18:WHFh4HNlvX6LyRitZD8Rm6KbrEGj0fMy:jD5odNEHEOIW7az2ekXH7hmj', 3, '3'),
+(3, 'Elinda', 'thayane', '999.999.999-99', '99999999999999', 'fndsihnf@iwfhfisu', '(99) 99999-9999', 'sha1:64000:18:F4zbnWCjGk1VZiWyvUXYVcsztD9l5I47:5S0ml34pMPxyjW8X4z474W5e', 3, '3'),
+(4, 'Elinda', 'thayane', '999.999.999-99', '99999999999999', 'fndsihnf@iwfhfisu', '(99) 99999-9999', 'sha1:64000:18:LVbqqIZd1VpQBBN79OZ/lcIkZdP07hhT:LMxO87zcV2DuwtVzP8Cea5hP', 3, '3'),
+(5, 'Sandra Loeps', 'sandra', '888.888.888-88', '8888888', 'sdd@sdFs', '(81) 98777-7777', '25f9e794323b453885f5181f1b624d0b', 4, '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `campus` (`campus`),
+  ADD KEY `tipo_usuario` (`tipo_usuario`),
+  ADD KEY `cpf` (`cpf`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
